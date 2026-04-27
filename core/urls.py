659b600c84100ad23406002f80b3f27c599f7c1d
kaijimiyashita-ai <kaiji.miyashita@gmail.com>
2026-04-27 02:34:43 -0400

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     # Override default token view with custom serializer
     path('token/', TokenObtainPairView.as_view(serializer_class=MyTokenObtainPairSerializer), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-   path('setup/', views.setup_users, name='setup_users'),
+    path('setup/', views.setup_users, name='setup_users'),
 ]
